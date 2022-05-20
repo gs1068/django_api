efc60a491195b11dea93242462bc96493c33acf9
gs1068 <genta.otias@gmail.com>
2022-05-20 18:02:39 +0900

    admin追加

--- to_do/models.py
+++ to_do/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+# 引数のmodels.Modelは固定で
 class Todo(models.Model):
     title = models.CharField(max_length=140, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # この記載があることで管理画面に表示されるモデル内のデータを判別している
+    # この記載がないと管理画面側ではtodo objectと表示される
     def __str__(self):
         return self.title
